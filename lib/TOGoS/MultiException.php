d47362e84b31101bfa6c58853834211c0fa12655
TOGoS <togos00@gmail.com>
2017-01-02 20:50:50 +0000

    Can now include arbitrary data with MultiExceptions.

--- lib/TOGoS/MultiException.php
+++ lib/TOGoS/MultiException.php
@@ -3,19 +3,27 @@
 class TOGoS_MultiException extends Exception
 {
 	protected $causes;
-	public function __construct( $message, $code, $causes ) {
+	protected $data;
+	
+	public function __construct( $message, $code=0, $causes=array(), $data=array() ) {
 		$previous = null; foreach($causes as $previous) break;
 		parent::__construct($message, $code, $previous);
 		$this->causes = $causes;
+		$this->data = $data;
 	}
 	
 	public function getCauses() {
 		return $this->causes;
 	}
 	
-	public static function throwIfAny( $exceptions, $message="Some stuff went wrong" ) {
+	public function getData($key=null) {
+		if( $key === null ) return $this->data;
+		return isset($this->data[$key]) ? $this->data[$key] : null;
+	}
+	
+	public static function throwIfAny( $exceptions, $message="Some stuff went wrong", $data=array() ) {
 		if( count($exceptions) > 0 ) {
-			throw new self( $message, 0, $exceptions );
+			throw new self( $message, 0, $exceptions, $data );
 		}
 	}
 }
--- test/TOGoS/MultiExceptionTest.php
+++ test/TOGoS/MultiExceptionTest.php
@@ -27,6 +27,7 @@ class TOGoS_MultiExceptionTest extends PHPUnit_Framework_TestCase
 		$this->assertNotNull( $caught->getPrevious() );
 		$this->assertEquals( "Test", $caught->getPrevious()->getMessage() );
 		$this->assertEquals( 1, count($caught->getCauses()) );
+		$this->assertEmpty( $caught->getData() );
 	}
 
 	public function testThrowTwo() {
@@ -44,5 +45,32 @@ class TOGoS_MultiExceptionTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals( 2, count($cazzes) );
 		$this->assertEquals( "Test1", $cazzes[0]->getMessage() );
 		$this->assertEquals( "Test2", $cazzes[1]->getMessage() );
+	}
+	
+	public function testThrowWithData() {
+		$caught = null;
+		try {
+			TOGoS_MultiException::throwIfAny( array(new Exception("Test1")), "Sew mani sepshins", 'Frog pellets' );
+		} catch( TOGoS_MultiException $me ) {
+			$caught = $me;
+		}
+		$this->assertNotNull( $caught, "Should've thrown a MultiException!") ;
+		$this->assertEquals( 'Frog pellets', $caught->getData() );
+		$this->assertNull( $caught->getData('leafs') );
+	}
+	
+	public function testThrowWithMoreData() {
+		$caught = null;
+		$data = array('pellets'=>'Frogs', 'leafs'=>'Trees');
+		try {
+			TOGoS_MultiException::throwIfAny( array(new Exception("Test1")), "Sew mani sepshins", $data );
+		} catch( TOGoS_MultiException $me ) {
+			$caught = $me;
+		}
+		$this->assertNotNull( $caught, "Should've thrown a MultiException!") ;
+		$this->assertEquals( $data, $caught->getData() );
+		$this->assertEquals( 'Frogs', $caught->getData('pellets') );
+		$this->assertEquals( 'Trees', $caught->getData('leafs') );
+		$this->assertNull( $caught->getData('ostrichs') );
 	}
 }
